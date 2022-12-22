3c02368d04b6d4e70fb8de7ac4e9082f572e2ccf
icebw1 <117940991+icebw1@users.noreply.github.com>
2022-12-22 16:22:14 +0100

    Update eau07.py
    Exo corrigé

--- eau07.py
+++ eau07.py
@@ -1,26 +1,17 @@
 """
 Exercice : Majuscule
 (slide 10)
-
 Créez un programme qui met en majuscule la première lettre de chaque mot d’une chaîne de caractères. Les autres lettres devront être en minuscules. Les mots ne sont délimités que par un espace, une tabulation ou un retour à la ligne.
-
-
 Exemples d’utilisation :
 $> python exo.py “bonjour mathilde, comment vas-tu ?!”
 Bonjour Mathilde, Comment Vas-tu ?!
-
-
 $> python exo.py 42
 error
-
 Afficher error et quitter le programme en cas de problèmes d’arguments.
-
-
 """
 #Fonction
 def majuscule(t):
     i = 0                           #Indice qui parcourt le tableau
-    y = 0                           #Indice qui gère l'espace pour mettre le caractere à la Maj/min
 
     while i < len(t):
         if i == 0:
@@ -31,10 +22,9 @@ def majuscule(t):
 
         if t[i] == " ":
             print(end=t[i+1].upper()) 
+            i = i + 1  
 
-        i = i + 1  
-        y = y + 1
-         
+        i = i + 1        
 
 #Parsing
 x = input("Taper une phrase \n")
