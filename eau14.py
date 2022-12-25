a11798de79c66fd7d2a5746643d8be7c27975766
icebw1 <michel.colombano@gmail.com>
2022-12-25 16:43:47 +0100

    exo tri ASCII résolu

--- eau14.py
+++ eau14.py
@@ -4,37 +4,39 @@ Exercice : Par ordre Ascii
 
 Créez un programme qui trie les éléments donnés en argument par ordre ASCII.
 
-
 Exemples d’utilisation :
 $> python exo.py Alfred Momo Gilbert
 Alfred Gilbert Momo
-
 
 $> python exo.py A Z E R T Y
 A E R T Y Z
 
 Afficher error et quitter le programme en cas de problèmes d’arguments.
 
-
 """
 #Fonction
 
 def triParOrdreASCII(t):
     i = 0
-    while i < len(t)-1: #à 1
-            if t[i+1] < t[i]:
-                (t[i+1], t[i]) = (t[i], t[i+1])
-                i += 1
+    j = 0   
+    while i <= len(t)-j:     #Après le 1er parcous, le plus grand nb étant à sa position définitive (en dernier) : il n'a plus à être traité. 
+            if i == len(t)-1:
+                i = 0
+                j = j + 1
 
-            elif t[i+1] > t[i]:
-                (t[i+1], t[i]) = (t[i+1], t[i])
-                i += 1           
-    print(t)
+            if t[i] <= t[i+1]:
+                (t[i], t[i+1]) = (t[i], t[i+1])
 
+            elif t[i] > t[i+1]:
+                (t[i], t[i+1]) = (t[i+1], t[i])
+
+            i = i + 1
+    for n in t:
+        print(end=n + " ")
 
 try:
     #Parsing
-    x = input("\n")
+    x = input("Ce programme trie une liste de mots (Tri par ordre Ascii).\n")
     tableau = x.split(" ")
 
     #Affichage
